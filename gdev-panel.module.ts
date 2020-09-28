1ef80de1aa3b8b1da2864cad3723ea2f9700b235
Jorge Gu7man <46007655+jgu7man@users.noreply.github.com>
2020-09-28 05:48:47 -0500

    component creado"

--- gdev-panel.module.ts
+++ gdev-panel.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GdevPanelRoutingModule } from './gdev-panel-routing.module';
+import { PanelComponent } from './panel/panel.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PanelComponent],
   imports: [
     CommonModule,
     GdevPanelRoutingModule
